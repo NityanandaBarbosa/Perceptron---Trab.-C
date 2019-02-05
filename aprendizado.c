2e80c5d3f8e91f6d118da80432b3092a6d19dd35
André Câmara <andreluiscamara@gmail.com>
2019-02-05 13:36:21 -0300

    Organizando o código

--- aprendizado.c
+++ aprendizado.c
@@ -3,29 +3,37 @@
 int main(void)
 {
     char linha[100]; /* ou maior se vc precisar */
-    FILE *arquivo;
+    
     float x1[30],x2[30],y[30];
     float w1 = 1;
     float w2 = 1;
     float taxa = 0.01;
 
+    FILE *arquivo;
+    
     arquivo = fopen("A_l.txt", "r");
+    
     if(arquivo == NULL) {
         printf("arquivo desconhecido - nome_do_arquivo.txt\n");
         return 0;
     }
-    int i=0;
+    
+    int i = 0;
+    
     while(fgets(linha, sizeof(linha), arquivo) != NULL){
         fscanf(arquivo,"%f %f %f", &x1[i], &x2[i], &y[i]);
         i++;
     }
+    
     for(i=0; i<29; i++){
         printf("%.2f %.2f %.2f\n", x1[i],x2[i],y[i]);
     }
+    
     fclose(arquivo);
     
     int c = 0;
     float yr =0;
+    
     while(c!=30){
         yr = (w1*x1[i])+(w2*x2[i]);
         if(y >=0){
@@ -33,13 +41,8 @@ int main(void)
         }else{
             yr=0;
         }
-
-        
-
     }
 
-
-    
     return 0;
 
 }
