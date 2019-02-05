e6eb6e9c4f9b03db97ac8a05344a7987f43687c5
André Câmara <andreluiscamara@gmail.com>
2019-02-05 16:26:10 -0300

    Atualizando o código

--- aprendizado.c
+++ aprendizado.c
@@ -4,9 +4,10 @@ float definirW1deVetor();
 float definirW2deVetor();
 
 int main(void){
+
     char linha[100];
 
-    float x1[30],x2[30],y[30];
+    float x1[30], x2[30], y[30];
     float taxa = 0.01;
 
     FILE *arquivo;
@@ -28,25 +29,42 @@ int main(void){
         i++;
     }
     
-    for(i=0; i<30; i++){
-    printf("c=%d %.2f %.2f %.2f\n", i,x1[i],x2[i],y[i]);
+    for(i = 0; i < 30; i++){
+    printf("c=%d %.2f %.2f %.2f\n", i, x1[i], x2[i], y[i]);
     }
     
     fclose(arquivo);
     
     int c = 0;
-    float yr =0;
+    float yr = 0;
     
-    while(c!=30){
-        yr = (w1*x1[i])+(w2*x2[i]);
-        if(y >=0){
-            yr=1;
-        }else{
-            yr=0;
+    while(c < 30){
+        yr = (w1*x1[c]) + (w2*x2[c]);
+        
+        if(yr >= 0){
+            yr = 1;
         }
-    }
 
-return 0;
+        else if(yr < 0){
+            yr = 0;
+        }
+
+        float d = y[c] - yr;
+        
+        if(d == 0){
+            c++;
+        } else if (d != 0){
+            w1 = (taxa*d*x1[c]) + w1;
+            w2 = (taxa*d*x2[c]) + w2;      
+            c = 0;
+        }
+
+        yr = 0;
+        printf("%d  w1: %.2f w2: %.2f d:%.2f\n",c,w1,w2,d);
+    }
+    printf("Vetor W descoberto!\nw1:%.2f  w2:%.2f.",w1,w2);
+      
+      return 0;
 }
 
 float  definirW1deVetor(){
